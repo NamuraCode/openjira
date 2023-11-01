256111c849c2b4fe66c95aaa78cd368d4ae8052b
David <naranjo.david.1999@gmail.com>
2023-11-01 07:39:48 -0500

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,22 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,9 +1,14 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 interface layoutProps {
   children: ReactNode;
 }
 
 export const Layout = ({ children }: layoutProps) => {
-  return <main>{children}</main>;
+  return (
+    <main className="o">
+      <h2>layout</h2>
+      {children}
+    </main>
+  );
 };
