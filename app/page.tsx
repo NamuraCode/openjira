8e68e9908a6418ffe4db46fd1feeeb17f24791a0
David <naranjo.david.1999@gmail.com>
2023-11-27 22:53:59 -0500

    materia ui typhography

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Layout } from '@/layout';
+import { Typography } from '@mui/material';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 export default function HomePage() {
   return (
     <Layout>
-      <h1>Hola mundo</h1>
+      <Typography variant='h1'>Hello World</Typography>
     </Layout>
   );
 }
