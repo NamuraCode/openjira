efcc9e2c5e180aa0a587945122c05c4a1feba0eb
David <naranjo.david.1999@gmail.com>
2023-11-27 23:52:08 -0500

    some problems whit Head next

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,26 +1,15 @@
 import { Box } from "@mui/material";
 import Head from "next/head";
 import { ReactNode } from "react";
-import type { Metadata } from 'next'
 
 interface layoutProps {
   children: ReactNode;
   title: string;
 }
- 
-export function generateMetadata({ title }: layoutProps): Metadata {
-  return {
-    title: title,
-  }
-}
 
-export const Layout = ({ title = 'opernJira', children }: layoutProps) => {
-  
+export const Layout = async({ title = 'opernJira', children }: layoutProps) => {
   return (
     <Box sx={{flexFlow:1}}>
-      <Head>
-        <title>{title}</title>
-      </Head>
       <Box sx={{padding: '10px 20px' }}>
         {children}
       </Box>
