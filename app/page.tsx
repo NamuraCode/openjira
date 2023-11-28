6302efb6886b5070199fffcc6bae8c0692f5a69c
David <naranjo.david.1999@gmail.com>
2023-11-28 00:05:04 -0500

    rootlayout and customlayout

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
 import RootLayout from './layout';
 import { Typography } from '@mui/material';
-// import { Metadata } from 'next';
-
-// export const metadata: Metadata = {
-//   title: 'Home Page',
-//   description: 'dashboard open-jira',
-// };
 
 export default function HomePage() {
   return (
-    <RootLayout title='home'>
+    <RootLayout title='Home'>
       <Typography variant='h1'>Hello World</Typography>
     </RootLayout>
   );
