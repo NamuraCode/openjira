55e2de19c062244fb15e23c5035b35e9c1c4e3e3
David <naranjo.david.1999@gmail.com>
2023-12-01 00:14:00 -0500

    UiReducer

--- context/ui/UiReducer.ts
+++ context/ui/UiReducer.ts
@@ -1,3 +1,22 @@
-export const UiReducer = (): boolean =>{
-    return false
-}
+import { UiState } from ".";
+
+type UiActionType =
+  | { type: "Ui - Open Sidebar" }
+  | { type: "Ui - Close Sidebar" };
+
+export const UiReducer = (state: UiState, action: UiActionType): UiState => {
+  switch (action.type) {
+    case "Ui - Open Sidebar":
+      return {
+        ...state,
+        sideMenuOpen: true,
+      };
+    case "Ui - Close Sidebar":
+      return {
+        ...state,
+        sideMenuOpen: false,
+      };
+    default:
+        return state
+  }
+};
