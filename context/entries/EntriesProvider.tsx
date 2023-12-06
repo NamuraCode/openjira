550e89598872144505a4c9718c3d892c7e111bd8
David <naranjo.david.1999@gmail.com>
2023-12-05 23:55:53 -0500

    data

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,23 +1,49 @@
-import { ReactNode, useReducer } from 'react'
-import { EntriesContext, entriesReducer } from '.'
-import { Entry } from '@/interfaces'
+import { ReactNode, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import { Entry } from "@/interfaces";
+import { EntriesContext, entriesReducer } from ".";
 
 export interface EntriesState {
-    entries: Entry[]
+  entries: Entry[];
 }
 
-const ENTRIES_INITIAL_STATE : EntriesState = {
-    entries: []
-} 
+const ENTRIES_INITIAL_STATE: EntriesState = {
+  entries: [
+    {
+      _id: uuidv4(),
+      description:
+        "proident dolor duis elit sunt qui dolor laborum veniam ea laborus qui consequat",
+      status: "pending",
+      createdAt: Date.now(),
+    },
+    {
+      _id: uuidv4(),
+      description:
+        "proident dolor duis elit sunt qui dolor laborum veniam ea laborus qui consequat",
+      status: "in-progress",
+      createdAt: Date.now() - 1000000,
+    },
+    {
+      _id: uuidv4(),
+      description:
+        "Que sueño tan stenta hp",
+      status: "finished",
+      createdAt: Date.now() - 100000,
+    },
+  ],
+};
 
-export const EntriesProvider = ( children : ReactNode)=>{
-    const [ state, dispatch ] = useReducer(entriesReducer, ENTRIES_INITIAL_STATE) 
+export const EntriesProvider = (children: ReactNode) => {
+  const [state, dispatch] = useReducer(entriesReducer, ENTRIES_INITIAL_STATE);
 
-    return(
-        <EntriesContext.Provider value={{
-            ...state
-        }}>
-            {children}
-        </EntriesContext.Provider>
-    )
-}
+  return (
+    <EntriesContext.Provider
+      value={{
+        ...state,
+      }}
+    >
+      {children}
+    </EntriesContext.Provider>
+  );
+};
