d14e23fd8960d34f9e27af2f81c6c1471575cf0c
David <naranjo.david.1999@gmail.com>
2023-12-08 23:29:49 -0500

    EntryList and EntryCard

--- components/ui/EntryList.tsx
+++ components/ui/EntryList.tsx
@@ -1,7 +1,12 @@
+import { Paper } from "@mui/material"
+
 export const EntryList = () => {
 
     return (
-      <>
-       </>
+      <div  >
+        <Paper sx={{ height: 'calc(100vh - 250px)', overflow: 'scroll', backgroundColor: 'transparent'}}>
+
+        </Paper>
+       </div>
      )
   }
