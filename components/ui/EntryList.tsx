5cbeec02c44969ad067e8738c910e65747ee577b
David <naranjo.david.1999@gmail.com>
2023-12-11 23:50:25 -0500

    learn again next js the course was actualized

--- components/ui/EntryList.tsx
+++ components/ui/EntryList.tsx
@@ -1,12 +1,27 @@
-import { Paper } from "@mui/material"
+import { IconButton, List, ListItem, ListItemText, Paper } from "@mui/material";
 
 export const EntryList = () => {
-
-    return (
-      <div  >
-        <Paper sx={{ height: 'calc(100vh - 250px)', overflow: 'scroll', backgroundColor: 'transparent'}}>
-
-        </Paper>
-       </div>
-     )
-  }
+  return (
+    <div>
+      <Paper
+        sx={{
+          height: "calc(100vh - 250px)",
+          overflow: "scroll",
+          backgroundColor: "transparent",
+        }}
+      >
+        <List>
+          <ListItem
+            disableGutters
+            secondaryAction={
+              <IconButton aria-label="comment">
+              </IconButton>
+            }
+          >
+            <ListItemText primary={`Line item ${1}`} />
+          </ListItem>
+        </List>
+      </Paper>
+    </div>
+  );
+};
